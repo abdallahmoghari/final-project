3b11e86685b168f7d54b0777286522a17d80de6f
abdallamoghari <abdallamoghari2003@gmail.com>
2025-03-03 14:06:28 +0200

    3 scommit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use PHPUnit\Framework\MockObject\Stub\ReturnSelf;
+use Symfony\Component\VarDumper\Caster\RedisCaster;
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,4 +29,44 @@ Route::post('/about', function () {
         '3' => 'Sales',
     ];
     return view('about', compact('name', 'departments'));
+});
+
+Route::post('create', function() {
+    $task_name = $_POST['name'];
+    DB::table('tasks')->insert (['name' => $task_name]);
+    return view('tasks');
+    });
+
+Route::get('tasks', function(){
+
+    $tasks = DB::table('tasks')->get();
+
+    return view('tasks',compact('tasks'));
+
+
+});
+
+Route::post('create', function(){
+    $task_name = $_POST['name'];
+    DB::table('tasks')->insert (['name' => $task_name]);
+    return redirect()->back();
+    });
+
+Route::post('delete/{id}', function($id){
+
+    DB::table('tasks')->where('id', $id)->delete();
+    return redirect()->back();
+});
+
+
+Route::post('edit/{id}', function($id){
+    $task= DB::table('tasks')->where('id',$id)->first();
+    $tasks= DB::table('tasks')->get();
+    return view('tasks',compact('task','tasks'));
+    });
+
+Route::post('update',function(){
+    $id = $_POST['id'];
+    DB::table(table: 'tasks')->where('id', '=', $id)->update (['name' => $_POST['name']]);
+    return redirect('tasks');
 });
